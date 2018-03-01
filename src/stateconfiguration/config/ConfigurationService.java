2558c827428c6d5d009768a27b2ae08221fc1a89
Stanislav Arnaudov <stanislav_ts@abv.bg>
2018-03-01 20:04:26 +0100

    Minor

--- src/stateconfiguration/config/ConfigurationService.java
+++ src/stateconfiguration/config/ConfigurationService.java
@@ -24,22 +24,7 @@ public class ConfigurationService {
         linksFields = FXCollections.observableList(new LinkedList<Field>());
         
         
-        statesFields.add(new Field("Threshold1", Field.ValueType.Real));
-        statesFields.add(new Field("Threshold2", Field.ValueType.Real));
-        statesFields.add(new Field("Threshold3", Field.ValueType.Real));
-        statesFields.add(new Field("Threshold4", Field.ValueType.Real));
-        statesFields.add(new Field("Threshold5", Field.ValueType.Text));
-        statesFields.add(new Field("Threshold6", Field.ValueType.Integer));
-        statesFields.add(new Field("Threshold7", Field.ValueType.Integer));
-        statesFields.add(new Field("Threshold8", Field.ValueType.Integer));
-        statesFields.add(new Field("Threshold9", Field.ValueType.Integer));
-        statesFields.add(new Field("Threshold0", Field.ValueType.Integer));
-        statesFields.add(new Field("Threshold11", Field.ValueType.Integer));
-        statesFields.add(new Field("Threshold12", Field.ValueType.Integer));
-        statesFields.add(new Field("Threshold13", Field.ValueType.Integer));
-        statesFields.add(new Field("Threshold14", Field.ValueType.Integer));
-        statesFields.add(new Field("Threshold15", Field.ValueType.Integer));
-        
+
         
         
     }
